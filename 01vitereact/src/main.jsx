9246d15392c56dcf6285225a308c0b9d016b555c
Hassan Sadiq khan <hassansadiqkhan8@gmail.com>
2024-01-27 16:10:54 +0500

    update

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+    return (
+        <>
+            <h1>Say hello to the World again</h1>
+        </>
+    )
+}
+
+const variable = "Chai aur code"
+
+const reactElement = React.createElement(
+    "a",
+    {href: "https://www.google.com", target:"_blank"},
+    "Click me to visit google",
+    variable
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <App />
-  
+
+    reactElement
+
 )
