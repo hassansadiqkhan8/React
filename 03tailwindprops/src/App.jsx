ebfd4a3770d0a2ea69d0f99168af43fb79fcbc4e
Hassan Sadiq khan <hassansadiqkhan8@gmail.com>
2024-01-28 21:01:50 +0500

    tailwindbasics

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-5">Say Hello to the Tailwind</h1>
+      <Card username = "Jennifer" btnText = "Jennifer profile" />
+      <Card username = "Yennifer" btnText = "Yennifer profile" />
+    </>
+  )
+}
+
+export default App
